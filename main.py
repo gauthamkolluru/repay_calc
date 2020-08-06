00fd337e9b9da089674091c1968ddea890fde002
Gautham Kolluru <sakollur@cisco.com>
2020-08-06 22:39:38 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import json
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
-PRODUCTTEMPLATENAME = "products_template.json"
+# PRODUCTTEMPLATENAME = "products_template.json"
+
+INSTRUCTIONS = {
+    "Date format": "yyyy-mm-dd EX 2020-12-31 for 31st December 2020"
+}
 
 OPTIONS = {
     1: "New Data",
@@ -10,20 +14,17 @@ OPTIONS = {
 }
 
 PURCHASE_DETAILS = {
-    "ITEM_DETAILS": {
-        'name': "",
-        'bought from': "",
-        'bought on': "",
-        'bought using': "",
-        'price': "",
-    },
-    "REPAYMENT_DETAILS": {
-        "start date": "",
-        "tenure": "",
-        "end date": "",
-        "emi": ""
-    }
-
+    "item": [
+        'name',
+        'bought from',
+        'bought on',
+        'bought using',
+        'price',
+    ],
+    "repayment": [
+        "start date",
+        "tenure"
+    ]
 }
 
 
@@ -46,42 +47,56 @@ def write_json(data_dict, file_name):
     return False
 
 
-def read_json(file_name=PRODUCTTEMPLATENAME):
+def read_json(file_name):
     with open(file_name) as fn:
         return json.load(fn)
     return False
 
 
-def menu(options=OPTIONS):
-
-    for option in options:
-        print("Press {} for {}".format(option, options[option]))
-
-    return user_input("Press a key : ")
-
-
-# def gpd():
-#     """
-#     GPD : Get Product Details
-#     """
-#     return {i: input("Product {} : ".format(i)) for i in ITEM_DETAILS}
-
-
-def grd():
+def grd(pd):
     """
-    GRD : Get Repayment Details 
+    GRD : Get Repayment Details
     """
-    return {i: {j: input("{} {} : ".format('Product' if i.lower() == 'item_details' else 'Repayment', j)) for j in PURCHASE_DETAILS[i]} for i in PURCHASE_DETAILS}
+
+    for purchase in PURCHASE_DETAILS:
+        for item in PURCHASE_DETAILS[purchase]:
+            item_detail = input("{} {} : ".format(
+                'Product' if purchase.lower() == 'item' else 'Repayment', item))
+            pd.update({item: item_detail})
+    return pd
+
+
+def get_end_date(pd):
+    start_date = datetime.strptime(pd['start date'], "%Y-%m-%d")
+    no_of_days = int(pd['tenure']) * 30
+    end_date = start_date + timedelta(days=no_of_days)
+    end_date = datetime.strptime(
+        "{}-{}-{}".format(end_date.year, end_date.month, start_date.day), "%Y-%m-%d").date().isoformat()
+    pd.update({'end date': end_date})
+    return pd
 
 
 def order(choice):
     if choice == 1:
-        pd = grd()
+        pd = dict()
+        pd = grd(pd)
+        pd = get_end_date(pd)
         print(pd)
         return True
     if choice == 2:
         return True
     return False
+
+
+def menu():
+
+    for option in OPTIONS:
+        print("Press {} for {}".format(option, OPTIONS[option]))
+
+    for instruction in INSTRUCTIONS:
+        print(instruction + " " + INSTRUCTIONS[instruction])
+
+    return user_input("Press a key : ")
 
 
 def main():
